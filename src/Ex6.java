db59515efe4e35746d8ee3d36137871e38de06d8
itayamos <94404706+itayamos@users.noreply.github.com>
2021-12-20 09:54:35 +0200

    Ex3rd

--- src/Ex6.java
+++ src/Ex6.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class Ex6 {
     public static void main(String[] args) {
-
+        Scanner scanner= new Scanner(System.in);
+        String mathExp=scanner.nextLine();
+        System.out.println(mathExpression(mathExp));
+        System.out.println(Equation(mathExp));
     }
 
     public static boolean mathExpression(String mathExp) {
         boolean flag=true;
         int[] num={0,1,2,3,4,5,6,7,8,9};
         char[] sign={'+','-','*','/','^','(',')'};
-        for (int i=0; i<mathExp.length(); i++){
+        for (int i=0; i<mathExp.length() && mathExp.length()>2 ; i++){
             if (mathExp.charAt(0)==sign[2] || mathExp.charAt(0)==sign[3] || mathExp.charAt(0)==sign[4]){
                 flag=false;
                 break;
@@ -16,7 +21,7 @@ public class Ex6 {
                 flag=false;
                 break;
             }
-            for (int k=0; k<mathExp.length(); k++){
+            for (int k=0; k<mathExp.length() && mathExp.length()>3; k++){
                 if (mathExp.charAt(i)==sign[5]){
                     if (mathExp.charAt(k)==sign[6]){
                         if (i<k){
@@ -39,5 +44,37 @@ public class Ex6 {
             }
         }
         return flag;
+
+    }
+    public static double Equation(String mathExp){
+        double outcome = 0;
+        int[] num={0,1,2,3,4,5,6,7,8,9};
+        char[] sign={'+','-','*','/','^','(',')'};
+        for (int i=0; i<mathExp.length(); i++){
+            for (int j=0; j<num.length; j++){
+                for (int k=0; k< sign.length; k++){
+                    for (int p=0; p< num.length; p++)
+                    if (mathExp.charAt(i)==num[j] && mathExp.charAt(i+1)==sign[k] && mathExp.charAt(i+2)==num[p]){
+                        if (mathExp.charAt(i+1)==sign[0]){
+                            outcome=num[j]+ num[p];
+                        }
+                        else if (mathExp.charAt(i+1)==sign[1]){
+                            outcome=num[j]-num[p];
+                        }
+                        else if (mathExp.charAt(i+1)==sign[2]){
+                            outcome=num[j]*num[p];
+                        }
+                        else if (mathExp.charAt(i+1)==sign[3]){
+                            outcome=(double) num[j]/num[p];
+                        }
+                        else if (mathExp.charAt(i+1)==sign[4]){
+                            outcome=Math.pow(num[j],num[p]);
+                        }
+
+                    }
+                }
+            }
+        }
+        return outcome;
     }
 }
